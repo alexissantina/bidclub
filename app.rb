a171fc5c9e44ca462f0ece156020bed3c808b70e
Alexis Johnson <alexissmj@gmail.com>
2017-08-01 19:49:34 -0700

    reverting back to last good config

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ require_relative("lib/art")
 #require("./lib/task")
 
 get("/") do
-  @arts = {Art.all()}
+  @arts = Art.all()
 #  @art = Art.find(params.fetch('id'))
   erb(:index)
 end
 
 get('/art') do
-  @arts = {Art.all()}
+  @arts = Art.all()
   erb(:arts)
 end
 
@@ -28,19 +28,19 @@ post("/arts") do
   height = params.fetch('height')
   width = params.fetch('width')
   edition = params.fetch('edition')
-  art = {Art.new({:name=> name, :artist=> artist, :price=> price, :medium=> medium, :height=> height, :width=> width, :edition=> edition})}
+  art = Art.new({:name=> name, :artist=> artist, :price=> price, :medium=> medium, :height=> height, :width=> width, :edition=> edition})
   art.save()
   erb(:success) #:layout_options => { :views => 'views/layout' }
 end
 
 get('/art/:id/edit') do
-  @arts = {Art.all()}
-  @art = {Art.find(params.fetch('id'))}
+  @arts = Art.all()
+  @art = Art.find(params.fetch('id'))
   erb(:edit)
 end
 
 patch('/art/:id') do
-  @art = {Art.find(params.fetch('id'))}
+  @art = Art.find(params.fetch('id'))
   name = params.fetch('name')
   artist = params.fetch('artist')
   medium = params.fetch('medium')
@@ -49,11 +49,11 @@ patch('/art/:id') do
   width = params.fetch('width')
   height = params.fetch('height')
   @art.update({:name => name, :artist => artist, :medium => medium, :edition => edition, :price => price, :width => width, :height => height})
-  @arts = {Art.all()}
+  @arts = Art.all()
   erb(:success)
 end
 
 get('/art/:id') do
-  @art = {Art.find(params.fetch("id"))}
+  @art = Art.find(params.fetch("id"))
   erb(:art)
 end
